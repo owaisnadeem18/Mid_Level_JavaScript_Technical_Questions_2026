f57e14bced6060d87d97ddec7516e2da250187bd
owaisnadeem18 <owaisnadeem15@gmail.com>
2026-03-19 23:31:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,4 @@ console.log(4 + 34 + "56" +3)
 
 // Reason:
 // 4 + 34 is evaluated first, resulting in 38. Then "56" is concatenated to 38, resulting in "3856". Finally, 3 is concatenated to "3856", resulting in "38563". Because there is a string in the middle, all the numbers before it are added together first, and then the result is concatenated with the string and the remaining number.
+
