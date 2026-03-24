5027d933d0736785ffc0c271624a2a7471dcd3b3
owaisnadeem18 <owaisnadeem15@gmail.com>
2026-03-24 19:53:09 +0500

    Update MemoryQuestionsInJavaScript.js

--- MemoryQuestionsInJavaScript.js
+++ MemoryQuestionsInJavaScript.js
@@ -2,8 +2,8 @@ let a = 10;
 let b = a;
 b = 20;
 
-console.log(a); // 10 → 'a' is unaffected because b gets a copy of a's value in a separate memory location
-console.log(b); // 20 → 'b' is independent of 'a'
+// console.log(a); // 10 → 'a' is unaffected because b gets a copy of a's value in a separate memory location
+// console.log(b); // 20 → 'b' is independent of 'a'
 
 // Stack vs Heap in JavaScript:
 
@@ -19,4 +19,40 @@ console.log(b); // 20 → 'b' is independent of 'a'
 
 // Key Takeaway:
 // - Primitive types → copied by value → independent variables
-// - Reference types → copied by reference → shared memory
+// - Reference types → copied by reference → shared memory 
+
+// ⚡ Shallow vs Deep (Quick Difference)
+// Feature	Shallow Copy ❌	Deep Copy ✅
+// Top-level copy	✅ Yes	✅ Yes
+// Nested copy	❌ Reference	✅ New copy
+// Safe changes	❌ No	✅ Yes
+// Performance	⚡ Fast	🐢 Slower
+
+let user = {
+  name: "Owais",
+  address: {
+    city: "Karachi"
+  }
+};
+
+let copy = { ...user }; // shallow copy
+
+copy.name = "Ali"; // top level change
+copy.address.city = "Lahore"; // nested change
+
+// console.log(user.name);        // Owais ✅ (safe)
+// console.log(user.address.city); // Lahore ❌ (changed)
+
+console.log(copy)
+
+// Shallow Copy vs Deep Copy in JavaScript:
+
+// Shallow Copy:
+// - Creates a new object with the same top-level properties as the original.
+// - Nested objects are shared between the original and the copy (reference).
+// - Changes to nested objects affect both the original and the copy.
+
+// Deep Copy:
+// - Creates a new object with the same properties as the original, including nested objects.
+// - Nested objects are also copied, creating independent memory locations.
+// - Changes to nested objects in the copy do not affect the original.
