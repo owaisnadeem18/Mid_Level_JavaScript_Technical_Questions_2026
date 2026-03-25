03d76e13f35fb8608491b1923f1dc96434a60536
owaisnadeem18 <owaisnadeem15@gmail.com>
2026-03-25 15:06:10 +0500

    Update EventLoopTechnicalQuestions.js

--- EventLoopTechnicalQuestions.js
+++ EventLoopTechnicalQuestions.js
@@ -1,12 +1,12 @@
 // Question # 01 
 
-console.log("Start");
+// console.log("Start");
 
-setTimeout(() => {
-  console.log("Timeout");
-}, 0);
+// setTimeout(() => {
+//   console.log("Timeout");
+// }, 0);
 
-console.log("End");
+// console.log("End");
 
 // Output: 
     // Start
@@ -17,14 +17,52 @@ console.log("End");
 
 // (Promise vs setTimeout)
 
-console.log("Start");
+// console.log("Start");
 
-setTimeout(() => {
-  console.log("Timeout");
-}, 0);
+// setTimeout(() => {
+//   console.log("Timeout");
+// }, 0);
 
-Promise.resolve().then(() => {
-  console.log("Promise");
-});
+// Promise.resolve().then(() => {
+//   console.log("Promise");
+// });
 
-console.log("End");
+// console.log("End");
+
+// (Multiple Promises)
+// console.log("Start");
+
+// Promise.resolve().then(() => {
+//   console.log("P1");
+// });
+
+// Promise.resolve().then(() => {
+//   console.log("P2");
+// });
+
+// console.log("End");
+
+// Output:
+    // Start
+    // End
+    // P1
+    // P2
+    
+    
+// Timeouts inside Promises in JavaScript:
+    
+// console.log("Start");
+
+// Promise.resolve().then(() => {
+//   console.log("Promise");
+
+//   setTimeout(() => {
+//     console.log("Timeout inside Promise");
+//   }, 0);
+// });
+
+// setTimeout(() => {
+//   console.log("Timeout");
+// }, 0);
+
+// console.log("End");
