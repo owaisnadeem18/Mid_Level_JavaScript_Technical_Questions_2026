d9b22be044a00fea1d6eaa2f8c2ef7d398374319
owaisnadeem18 <owaisnadeem15@gmail.com>
2026-04-21 07:07:47 +0500

    Update logical_questions_in_JS.js

--- logical_questions_in_JS.js
+++ logical_questions_in_JS.js
@@ -1,11 +1,39 @@
 // Question # 01 
-// Count Occurrences
+// Count Occurrences 
 // let arr = ["a", "b", "a", "c", "b", "a"];
+
+// let arr = ["a", "b", "a", "c", "b", "a"];
+
+// const countOccurences = (array) => {
+//     let counts = {}
+
+//     for (let i = 0 ; i < array.length ; i++) {
+        
+//         let element = array[i]
+
+//         counts[element] = (counts[element] || 0) + 1
+
+//     }
+
+//     return counts
+
+// }
+
+// console.log(countOccurences(arr))
+
+// Output:
+// { a: 3, b: 2, c: 1 }
+
+
+// Now also find out which is the most frequent element in the array , and it's max count:
 
 let arr = ["a", "b", "a", "c", "b", "a"];
 
 const countOccurences = (array) => {
+
     let counts = {}
+    let maxCount = 0
+    let mostFreq = ""
 
     for (let i = 0 ; i < array.length ; i++) {
         
@@ -15,12 +43,19 @@ const countOccurences = (array) => {
 
     }
 
-    return counts
+    for (let i in counts) {
+
+        if (counts[i] > maxCount) {
+            mostFreq = i
+            maxCount  = counts[i]
+        } 
+
+    }
+
+    return `The most frequent element in array is => "${mostFreq}" and it comes "${maxCount}" times in the array`
+
+
 
 }
 
 console.log(countOccurences(arr))
-
-// Output:
-// { a: 3, b: 2, c: 1 }
-
