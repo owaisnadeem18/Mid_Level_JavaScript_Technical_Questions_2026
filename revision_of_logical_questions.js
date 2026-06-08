5012bb6a485ade21d5053fa855c374f3946b8487
owaisnadeem18 <owaisnadeem15@gmail.com>
2026-06-08 13:50:59 +0500

    hashmap frequency

--- revision_of_logical_questions.js
+++ revision_of_logical_questions.js
@@ -1,26 +1,66 @@
-const arr = [
- {id:1,name:"A"},
- {id:2,name:"B"},
- {id:1,name:"A"}
-]
+// const arr = [
+//  {id:1,name:"A"},
+//  {id:2,name:"B"},
+//  {id:1,name:"A"}
+// ]
 
-const removeDuplicates = (array) => {
-    const seenIds = {}
+// const removeDuplicates = (array) => {
+//     const seenIds = {}
 
-    const result = []
+//     const result = []
+
+//     for (let i = 0 ; i < array.length ; i++) {
+//         const item = array[i]
+
+//         if (!seenIds[item?.id]) {
+//             seenIds[item.id] = true 
+//             result.push(item)
+//         } 
+
+//     }
+
+//     return result
+
+// }
+
+// console.log(removeDuplicates(arr))
+
+// ❓ Question 2: Find Count of Occurrences
+
+// Problem:
+// Given an array of integers, find the count of each element using a HashMap.
+
+// Example:
+// Input:
+// arr = [1, 2, 2, 3, 1, 4, 2]
+
+// Output:
+// 1 → 2
+// 2 → 3
+// 3 → 1
+// 4 → 1
+
+arr = [1, 2, 2, 3, 1, 4, 2]
+
+const countOcc = (array) => {
+    const occ = {}
 
     for (let i = 0 ; i < array.length ; i++) {
+        
         const item = array[i]
 
-        if (!seenIds[item?.id]) {
-            seenIds[item.id] = true 
-            result.push(item)
-        } 
+        if (!occ[item]) {
+            occ[item] = 1
+        }
+
+        else {
+            occ[item]++
+        }
 
     }
 
-    return result
+    return Object.entries(occ)
 
 }
 
-console.log(removeDuplicates(arr))
+console.log(countOcc(arr))
