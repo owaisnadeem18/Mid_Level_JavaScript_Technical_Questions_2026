2b3717b652d7bfa4611a60cfc2769dce9957246b
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2026-07-06 17:58:56 +0500

    object technical question

---
+++ objectsQuestions.js
@@ -0,0 +1,41 @@
+const orders = [
+  { customerId: 1, amount: 500, status: "paid" },
+  { customerId: 2, amount: 300, status: "pending" },
+  { customerId: 3, amount: 300},
+  { customerId: 1, amount: 700, status: "paid" },
+  { customerId: 2, amount: 400, status: "paid" }
+];
+
+// Expected output:
+
+// {
+//   1: 1200,
+//   2: 400
+// }
+
+const totalAmount = (array) => {
+    const obj = {}
+    let id 
+
+    for (let i = 0 ; i < array.length ; i++) {
+        
+        if (array[i]?.status == "paid") {
+        
+            
+            id = array[i]?.customerId
+
+        if (!obj[id]) {
+            obj[id] = 0
+        }
+
+        obj[id] += array[i]?.amount
+        
+        }
+
+    }
+
+    return obj
+
+}
+
+console.log(totalAmount(orders))
