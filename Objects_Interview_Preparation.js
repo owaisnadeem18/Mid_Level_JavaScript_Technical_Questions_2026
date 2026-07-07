601574160707ce27f7feaaaccb6af0735c33cce7
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2026-07-07 12:29:28 +0500

    count occurences of fruits

--- Objects_Interview_Preparation.js
+++ Objects_Interview_Preparation.js
@@ -18,7 +18,26 @@ const fruits = [
 // solution:
 
 const countApples = (arr) => {
-    return arr
+    
+    let obj = {}
+    let key
+
+    for (let i = 0 ; i<arr.length ; i++) {
+
+        key = arr[i]
+
+        if (!obj[key]) {
+            obj[key] = 1
+        }
+
+        else {
+            obj[key]++
+        }
+
+    }
+
+    return obj
+
 }
 
 console.log(countApples(fruits))
