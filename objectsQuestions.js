5930a8ce1dae4f037b8bf6aa65bb4f5e8610c640
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2026-07-07 12:49:03 +0500

    Update objectsQuestions.js

--- objectsQuestions.js
+++ objectsQuestions.js
@@ -1,10 +1,10 @@
-const orders = [
-  { customerId: 1, amount: 500, status: "paid" },
-  { customerId: 2, amount: 300, status: "pending" },
-  { customerId: 3, amount: 300},
-  { customerId: 1, amount: 700, status: "paid" },
-  { customerId: 2, amount: 400, status: "paid" }
-];
+// const orders = [
+//   { customerId: 1, amount: 500, status: "paid" },
+//   { customerId: 2, amount: 300, status: "pending" },
+//   { customerId: 3, amount: 300},
+//   { customerId: 1, amount: 700, status: "paid" },
+//   { customerId: 2, amount: 400, status: "paid" }
+// ];
 
 // Expected output:
 
@@ -71,28 +71,72 @@ const orders = [
 
 // again practice of same question:
 
+// const totalAmount = (arr) => {
+    
+//     let obj = {}
+//     let id 
+
+//     arr.forEach((item) => {
+        
+//         if (item?.status?.toLowerCase() == "paid") {
+//             id = item?.customerId
+
+//             if (!obj[id]) {
+//                 obj[id] = 0
+//             }
+
+//             obj[id] += item?.amount
+
+//         }
+
+//     })
+
+//     return obj
+
+// }
+
+// console.log(totalAmount(orders))
+
+// without status of "paid"
+
+const orders = [
+  { customerId: 1, amount: 500 },
+  { customerId: 2, amount: 300 },
+  { customerId: 1, amount: 700 },
+  { customerId: 3, amount: 200 },
+  { customerId: 2, amount: 400 }
+];
+
+
+// Expected Output:
+
+// {
+//   1: 1200,
+//   2: 700,
+//   3: 200
+// }
+
 const totalAmount = (arr) => {
     
-    let obj = {}
-    let id 
-
+    let obj= {}
+    let id
+    let iteration = 0
+    
     arr.forEach((item) => {
-        
-        if (item?.status?.toLowerCase() == "paid") {
-            id = item?.customerId
-
-            if (!obj[id]) {
-                obj[id] = 0
-            }
-
-            obj[id] += item?.amount
-
+        id = item?.customerId
+        iteration++
+        console.log(`iteration : ${iteration}`)
+        console.log("id is => " , id)
+        if (!obj[id]) {
+            obj[id] = 0
         }
-
-    })
+        
+        obj[id] += item?.amount
+        
+    } )
 
     return obj
-
+    
 }
 
 console.log(totalAmount(orders))
