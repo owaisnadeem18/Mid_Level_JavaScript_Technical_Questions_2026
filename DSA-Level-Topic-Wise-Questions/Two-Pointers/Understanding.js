a6da97cb7f4335ad92fd4f55e994b000b700065d
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2026-08-22 19:57:03 +0500

    Create Understanding.js

---
+++ DSA-Level-Topic-Wise-Questions/Two-Pointers/Understanding.js
@@ -0,0 +1,2 @@
+// The two pointers technique is a computer programming strategy. It uses two index variables to travel through a data list (like an array or text string) at the same time. This method helps solve problems faster by cutting down extra loops. 
+
